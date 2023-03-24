98603c01f9926c56b18e3ba2920621ba6aa4c381
AlbertBeweb <contact@albert-nicolas.fr>
2023-03-24 18:42:31 +0100

    Add banner

--- config/bundles.php
+++ config/bundles.php
@@ -60,4 +60,6 @@ return [
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Odiseo\SyliusBannerPlugin\OdiseoSyliusBannerPlugin::class => ['all' => true],
 ];
